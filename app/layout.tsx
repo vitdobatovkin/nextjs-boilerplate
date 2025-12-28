d0cadf114cd2c53d25e5744597bd95be13953773
vitdobatovkin <119784502+vitdobatovkin@users.noreply.github.com>
2025-12-28 17:25:22 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
-export const metadata = {
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
   title: "How based are you in 2026?",
   description: "Tap Based me — quick spin, and we’ll rate how based you are."
 };
