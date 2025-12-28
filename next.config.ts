a1823d4f8d9a9170e3382fb6f1dbdea6d716bb71
vitdobatovkin <119784502+vitdobatovkin@users.noreply.github.com>
2025-12-28 17:17:30 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "pbs.twimg.com" },
+      { protocol: "https", hostname: "abs.twimg.com" },
+      { protocol: "https", hostname: "x.com" }
+    ]
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
